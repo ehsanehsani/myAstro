ec8a25c90db1669f76c0bcaa34ac022b27ca5159
ehsan <pc.ehsan@gmail.com>
2025-05-29 17:07:04 +0200

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ehsanehsani.github.io/myAstro/', // this will match your GitHub Pages URL
+	base: '/myAstro/',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
